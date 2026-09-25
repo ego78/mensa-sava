65ab0e73d41966d774465e519caacb6539e6e157
ego78 <ego.giovanni@gmail.com>
2026-09-25 19:03:30 +0200

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,3 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
-android { namespace="it.egolab.mensasava"; compileSdk=35; defaultConfig { applicationId="it.egolab.mensasava"; minSdk=24; targetSdk=35; versionCode = 4; versionName = "3.1" }; compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }; kotlinOptions { jvmTarget="17" } }
+android { namespace="it.egolab.mensasava"; compileSdk=35; defaultConfig { applicationId="it.egolab.mensasava"; minSdk=24; targetSdk=35; versionCode = 5; versionName = "3.2" }; compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }; kotlinOptions { jvmTarget="17" } }
 dependencies { implementation("androidx.core:core-ktx:1.15.0"); implementation("androidx.appcompat:appcompat:1.7.0"); implementation("com.google.android.material:material:1.12.0"); implementation("androidx.browser:browser:1.8.0") }
